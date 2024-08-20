9e099be7015797b70b82a42d38e166df31affd7e
Yogesh Vashisth <writetokhair@gmail.com>
2024-08-20 16:59:05 +0530

    routing

---
+++ 02. routing-demo/src/app/products/[productId]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProductDetails({ params }: any) {
+  return <div>Details {params.productId}</div>;
+}
---
+++ 02. routing-demo/src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ProductList() {
+  return (
+    <div>
+      <h1>Product List</h1>
+      <h2>P1</h2>
+      <h2>P2</h2>
+      <h3>P3</h3>
+    </div>
+  );
+}
